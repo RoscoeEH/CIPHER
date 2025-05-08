a56ed7b03d917b44e904eb88aef7db83f609cbbe
RoscoeEH <elingshaynier@gmail.com>
2025-05-07 18:01:03 -0700

    Separated key derivation into another file

---
+++ src/key_derivation.rs
@@ -0,0 +1,50 @@
+use argon2::{Algorithm, Argon2, Params, Version};
+use pbkdf2::pbkdf2_hmac;
+use sha2::Sha256;
+
+pub fn argon2_derive_key(
+    password: &str,
+    salt: &[u8],
+    dklen: usize,
+    mem_cost: Option<u32>,
+    t_cost: Option<u32>,
+    par: Option<u32>,
+) -> Vec<u8> {
+    // Default values
+    let memory_cost = match mem_cost {
+        Some(n) => n,
+        None => 256 * 1024,
+    };
+    let time_cost = match t_cost {
+        Some(n) => n,
+        None => 8,
+    };
+    let parallelism = match par {
+        Some(n) => n,
+        None => 4,
+    };
+
+    let params = Params::new(memory_cost, time_cost, parallelism, Some(dklen))
+        .expect("Invalid Argon2 parameters");
+
+    let argon2 = Argon2::new(Algorithm::Argon2id, Version::V0x13, params);
+
+    let mut key = vec![0u8; dklen];
+    argon2
+        .hash_password_into(password.as_bytes(), salt, &mut key)
+        .expect("Argon2 hashing failed");
+
+    key
+}
+
+pub fn pbkdf2_derive_key(password: &str, salt: &[u8], dklen: usize, iters: Option<u32>) -> Vec<u8> {
+    let iterations = match iters {
+        Some(n) => n,
+        None => 100_000,
+    };
+
+    let mut key = vec![0u8; dklen];
+    pbkdf2_hmac::<Sha256>(password.as_bytes(), salt, iterations, &mut key);
+
+    key
+}

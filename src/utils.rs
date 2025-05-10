b8137e505d5ff707f50e9cbe976a2c479a6bed00
RoscoeEH <elingshaynier@gmail.com>
2025-05-10 10:59:58 -0700

    added time functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use crate::constants::*;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use std::error::Error;
 
 pub fn alg_name_to_id(name: &str) -> Result<u8, Box<dyn Error>> {
@@ -23,4 +24,20 @@ pub fn alg_id_to_name(id: u8) -> &'static str {
         PBKDF2_ID => "pbkdf2",
         _ => "unknown",
     }
+}
+
+/// Returns the current time as a UNIX timestamp (seconds since epoch).
+pub fn now_as_u64() -> u64 {
+    Utc::now().timestamp() as u64
+}
+
+/// Converts a DateTime<Utc> to a UNIX timestamp (u64).
+pub fn datetime_to_u64(datetime: DateTime<Utc>) -> u64 {
+    datetime.timestamp() as u64
+}
+
+/// Converts a UNIX timestamp (u64) to a DateTime<Utc>.
+pub fn u64_to_datetime(ts: u64) -> DateTime<Utc> {
+    // Safe as ts is from chrono timestamps, and this returns Result
+    DateTime::<Utc>::from_timestamp(ts as i64, 0).expect("Invalid UNIX timestamp value")
 }

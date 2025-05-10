a635e3132e1931a7fd74c65ff860b88f32ff9210
RoscoeEH <elingshaynier@gmail.com>
2025-05-10 10:58:29 -0700

    added aym crypto

---
+++ src/asymmetric_crypto.rs
@@ -0,0 +1,200 @@
+use elliptic_curve::{
+    pkcs8::{DecodePrivateKey, EncodePrivateKey},
+    sec1::{EncodedPoint, FromEncodedPoint, ToEncodedPoint},
+};
+
+use p256::{
+    ecdh::{diffie_hellman, EphemeralSecret},
+    ecdsa::{
+        signature::{Signer as EcdsaSigner, Verifier as EcdsaVerifier},
+        Signature as EcdsaSignature, SigningKey as EcdsaSigningKey,
+        VerifyingKey as EcdsaVerifyingKey,
+    },
+    PublicKey as P256PublicKey, SecretKey as P256SecretKey,
+};
+use rand::thread_rng;
+use rand_core::OsRng;
+use rsa::{
+    pkcs1::{DecodeRsaPrivateKey, DecodeRsaPublicKey, EncodeRsaPrivateKey, EncodeRsaPublicKey},
+    pkcs1v15::{
+        Signature as RsaSignature, SigningKey as RsaSigningKey, VerifyingKey as RsaVerifyingKey,
+    },
+    signature::{RandomizedSigner, SignatureEncoding},
+    Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey,
+};
+use sha2::{Digest, Sha256};
+use std::error::Error;
+
+use crate::constants::*;
+use crate::random::*;
+use crate::symmetric_encryption::*;
+
+// RSA
+pub fn rsa_key_gen(bits: usize) -> (Vec<u8>, Vec<u8>) {
+    let mut rng = thread_rng();
+    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("RSA key gen failed");
+    let public_key = RsaPublicKey::from(&private_key);
+    (
+        private_key
+            .to_pkcs1_der()
+            .expect("Failed to encode private key")
+            .as_bytes()
+            .to_vec(),
+        public_key
+            .to_pkcs1_der()
+            .expect("Failed to encode public key")
+            .as_bytes()
+            .to_vec(),
+    )
+}
+
+pub fn rsa_enc(pub_key: &[u8], data: &[u8]) -> Vec<u8> {
+    let public_key = RsaPublicKey::from_pkcs1_der(pub_key).expect("Failed to parse DER public key");
+
+    public_key
+        .encrypt(&mut thread_rng(), Pkcs1v15Encrypt, data)
+        .expect("RSA encryption failed")
+}
+
+pub fn rsa_dec(priv_key: &[u8], ciphertext: &[u8]) -> Vec<u8> {
+    let private_key =
+        RsaPrivateKey::from_pkcs1_der(priv_key).expect("Failed to parse DER private key");
+
+    private_key
+        .decrypt(Pkcs1v15Encrypt, ciphertext)
+        .expect("RSA decryption failed")
+}
+
+pub fn rsa_sign(priv_key: &[u8], data: &[u8]) -> Vec<u8> {
+    let private_key =
+        RsaPrivateKey::from_pkcs1_der(priv_key).expect("Failed to parse DER private key");
+
+    // Create a PKCS#1 v1.5 signing key
+    let signing_key = RsaSigningKey::<sha2::Sha256>::new(private_key.clone());
+
+    let mut rng = thread_rng();
+    let signature = signing_key.sign_with_rng(&mut rng, data);
+
+    signature.to_vec()
+}
+
+pub fn rsa_verify(
+    pub_key: &[u8],
+    data: &[u8],
+    signature: &[u8],
+) -> Result<(), rsa::signature::Error> {
+    let public_key = RsaPublicKey::from_pkcs1_der(pub_key).expect("Failed to parse DER public key");
+    let verifying_key = RsaVerifyingKey::<Sha256>::new(public_key.clone());
+    let sig = RsaSignature::try_from(signature)?;
+
+    // Returns Err(_) if the signature is invalid
+    verifying_key.verify(data, &sig)
+}
+
+// ECC
+
+pub fn ecc_key_gen() -> (Vec<u8>, Vec<u8>) {
+    // Generate a secret key
+    let secret = P256SecretKey::random(&mut OsRng);
+
+    // Derive public key from secret
+    let public = P256PublicKey::from_secret_scalar(&secret.to_nonzero_scalar());
+
+    // Serialize keys
+    let private_der = secret.to_pkcs8_der().unwrap().as_bytes().to_vec();
+    let public_bytes = public.to_encoded_point(false).as_bytes().to_vec(); // uncompressed
+
+    (private_der, public_bytes)
+}
+
+pub fn ecc_enc(pub_key: &[u8], data: &[u8], sym_alg_id: u8) -> Result<Vec<u8>, Box<dyn Error>> {
+    let encoded_point =
+        EncodedPoint::<p256::NistP256>::from_bytes(pub_key).expect("Invalid SEC1 public key bytes");
+
+    let public_key =
+        P256PublicKey::from_encoded_point(&encoded_point).expect("Invalid encoded point for P256");
+
+    // Ephemeral ECDH key exchange
+    let ephemeral = EphemeralSecret::random(&mut OsRng);
+    let shared_secret = ephemeral.diffie_hellman(&public_key);
+
+    // Derive symmetric key
+    let key_material = Sha256::digest(shared_secret.raw_secret_bytes());
+    let key = &key_material[..32];
+
+    // Encrypt symmetric
+    let nonce = get_nonce();
+    let ciphertext = id_encrypt(sym_alg_id, key, &nonce, data);
+    // Prepend ephemeral public key to ciphertext
+    let eph_pub = p256::PublicKey::from(&ephemeral);
+    let mut result = eph_pub.to_encoded_point(false).as_bytes().to_vec();
+    result.extend_from_slice(&ciphertext.unwrap());
+    Ok(result)
+}
+
+pub fn ecc_dec(
+    priv_key: &[u8],
+    ciphertext: &[u8],
+    sym_alg_id: u8,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let private_key = P256SecretKey::from_pkcs8_der(priv_key)
+        .map_err(|e| format!("invalid private key: {}", e))?;
+
+    // Split out ephemeral pubkey (65 bytes for uncompressed)
+    let eph_pub_bytes = &ciphertext[..65];
+    let actual_ciphertext = &ciphertext[65..];
+
+    let eph_pub = P256PublicKey::from_sec1_bytes(eph_pub_bytes)
+        .map_err(|e| format!("invalid ephemeral pubkey: {}", e))?;
+
+    let shared_secret = diffie_hellman(private_key.to_nonzero_scalar(), eph_pub.as_affine());
+
+    let key_material = Sha256::digest(shared_secret.raw_secret_bytes());
+    let key = &key_material[..32];
+
+    id_decrypt(sym_alg_id, key, actual_ciphertext)
+        .map_err(|e| format!("decryption failed: {}", e).into())
+}
+
+pub fn ecdsa_sign(priv_key: &[u8], data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let private_key = P256SecretKey::from_pkcs8_der(priv_key)
+        .map_err(|e| format!("invalid private key: {}", e))?;
+
+    // Convert to signing key
+    let signing_key = EcdsaSigningKey::from(private_key.clone());
+
+    // RFC6979 deterministic signing
+    let signature: EcdsaSignature = signing_key.sign(data);
+
+    Ok(signature.to_der().as_bytes().to_vec())
+}
+
+pub fn ecdsa_verify(
+    pub_key: &[u8],
+    data: &[u8],
+    sig_bytes: &[u8],
+) -> Result<(), p256::ecdsa::Error> {
+    let encoded_point =
+        EncodedPoint::<p256::NistP256>::from_bytes(pub_key).expect("Invalid SEC1 public key bytes");
+
+    let public_key =
+        P256PublicKey::from_encoded_point(&encoded_point).expect("Invalid encoded point for P256");
+
+    // Convert to verifying key
+    let verifying_key = EcdsaVerifyingKey::from(public_key.clone());
+
+    let signature = EcdsaSignature::from_der(sig_bytes)?;
+
+    verifying_key.verify(data, &signature)
+}
+
+pub fn id_keypair_gen(
+    alg_id: u8,
+    bits: Option<usize>,
+) -> Result<(Vec<u8>, Vec<u8>), Box<dyn std::error::Error>> {
+    match alg_id {
+        ECC_ID => Ok(ecc_key_gen()),
+        RSA_ID => Ok(rsa_key_gen(bits.unwrap_or(4096))),
+        _ => Err(format!("Unrecognized asymmetric key type: {}", alg_id).into()),
+    }
+}
